5c88b7b2df8210a1c4c240a4a54d1e2c06f85d0e
Tymon0399 <145258543+Tymon0399@users.noreply.github.com>
2023-09-17 11:59:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-
+meme_dict = {
+            "CRINGE": "Coś wyjątkowo dziwnego lub zawstydzającego",
+            "LOL": "Częsta reakcja na coś zabawnego",
+            "ROFL": "odpowiedź na żart",
+            "SHEESH": "lekka dezaprobata",
+            "CREEPY": "straszny, złowieszczy",
+            "AGGRO": "stać się agresywnym/zły",
+            }
+            
+            
+word = input("Wpisz słowo, którego nie rozumiesz (używaj wielkich liter!): ")
+if word in meme_dict.keys():
+    print(word, ':', meme_dict[word])
+else:
+    print('Nie ma takiego słowa!')
